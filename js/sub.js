e1288bc3bf94c6aadcf5f2b0ece9874b7050d31b
heosure <ggoolldd2309@gmail.com>
2023-11-30 13:38:13 +0900

    version1

--- js/sub.js
+++ js/sub.js
@@ -1,6 +1,5 @@
 const $onTitle = document.querySelectorAll(".method > div > div h4");
 const $arrow = document.querySelectorAll(".method > div > div h4 i");
-console.log($onTitle);
 
 $onTitle.forEach((title) => {
   title.addEventListener("click", (e) => {
